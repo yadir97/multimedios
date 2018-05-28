5da9980b3f0b3b3a8cb5afcdaac7b92cb8d9d6fc
yadir97 <yadir97.ya@gmail.com>
2018-05-28 00:58:21 -0600

    yadir-3-commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,21 @@
+function info_prox(i,button) {
+    	if($(".info-prox").eq(i).css('display')=='none'){
+    		$(".info-prox").eq(i).slideDown(1000);
+    		$(button).text("Cerrar");
+    	}else{
+    		$(".info-prox").eq(i).slideUp(1000);
+    		$(button).text("Ver mas");
+    	}
+    };
+
 $(document).ready(function() {
 	
     $("#carousel").carousel({
     	interval:3000,
 
     })
-	
+
+    
+    
+
 });
