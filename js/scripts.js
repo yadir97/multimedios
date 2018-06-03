fb3d4b7db8c77811678f337624f7d596c3853998
yadir97 <yadir97.ya@gmail.com>
2018-06-03 15:27:09 -0600

    yadir-commit-5

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,53 @@
 function info_prox(i,button) {
     	if($(".info-prox").eq(i).css('display')=='none'){
-    		$(".info-prox").eq(i).slideDown(1000);
+    		$(".info-prox").eq(i).slideDown(500);
     		$(button).text("Cerrar");
     	}else{
-    		$(".info-prox").eq(i).slideUp(1000);
+    		$(".info-prox").eq(i).slideUp(500);
     		$(button).text("Ver mas");
     	}
     };
 
+
+
 $(document).ready(function() {
 	
+	  $(".card-prox-parent").hide();
+
     $("#carousel").carousel({
     	interval:3000,
 
     })
 
+   $(window).resize(function(){
+   		if(window.matchMedia('(max-width:370px)').matches){
+   			$(".row-coleccion>div>section>p").hide("fast");
+   			$(".row-coleccion").css("height","auto");
+   		}
+   		if(window.matchMedia('(min-width:371px)').matches){
+   			$(".row-coleccion>div>section>p").fadeIn(1000);
+   			
+   		}
+   });
+
+   $(window).scroll(function(){
+   	 var conf_card_prox = false;		
+   		if($(window).scrollTop()>2200 && !conf_card_prox ){
+   			var r={};
+   			$(".card-prox-parent").show(	1000,function(){
+   				$(".row-prox").css('height', 'auto');	
+   			});
+   			
+   		
+   			conf_card_prox = true;
+   		}
+   });
+
+ 
+
+
     
     
 
 });
+
